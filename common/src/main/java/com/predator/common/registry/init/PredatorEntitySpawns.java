97a05c380e7f000d405dd694c50f00d7c7ecec7a
bvanseg <bvanseg@gmail.com>
2025-12-14 04:20:06 -0500

    🐞 Restored yautja jungle spawns.
    Signed-off-by: = <bvanseg@gmail.com>

--- common/src/main/java/com/predator/common/registry/init/PredatorEntitySpawns.java
+++ common/src/main/java/com/predator/common/registry/init/PredatorEntitySpawns.java
@@ -1,20 +1,26 @@
 package com.predator.common.registry.init;
 
+import com.blib.common.gameplay.model.spawning.BLibEntitySpawnData;
 import com.blib.common.gameplay.model.spawning.SpawnSettings;
+import com.blib.common.registry.impl.BLibEntityTypeRegistry;
+import com.predator.Predator;
+import com.predator.common.gameplay.entity.living.yautja.YautjaSpawning;
+import net.minecraft.tags.BiomeTags;
 
 public class PredatorEntitySpawns {
+
+    private static final BLibEntityTypeRegistry REGISTRY = Predator.MOD.createEntityTypeRegistry();
 
     public static void initialize() {
         // TODO: Move this back to config.
         var yautjaSpawn = new SpawnSettings(true, 1, 1, 10);
 
-        // FIXME:
-        // Services.REGISTRY.registerEntitySpawnData(
-        // BLibEntitySpawnData.builder(PredatorEntityTypes.YAUTJA)
-        // .withBiomeTagKey(BiomeTags.IS_JUNGLE)
-        // .withSpawnPredicate(YautjaSpawning.PREDICATE)
-        // .withSpawnSettings(yautjaSpawn)
-        // .build()
-        // );
+        REGISTRY.registerSpawnData(
+            BLibEntitySpawnData.builder(PredatorEntityTypes.YAUTJA)
+                .withBiomeTagKey(BiomeTags.IS_JUNGLE)
+                .withSpawnPredicate(YautjaSpawning.PREDICATE)
+                .withSpawnSettings(yautjaSpawn)
+                .build()
+        );
     }
 }
